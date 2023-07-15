2d7bceb0997907be54eea7f66e9efc9cfaf3dfe2
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-07-15 13:27:39 +0300

    Store: footer, main-nav, utils for tailwind etc.

--- ecommerce-store/app/layout.tsx
+++ ecommerce-store/app/layout.tsx
@@ -1,6 +1,8 @@
+import Footer from '@/components/footer'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Urbanist } from 'next/font/google'
+import Navbar from '@/components/navbar'
 
 const font = Urbanist({ subsets: ['latin'] })
 
@@ -17,7 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   )
